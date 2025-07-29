bb2388dab820bbb6c696b1a10be940d7369c8ed1
Georgi <givanov@cbtw.tech>
2025-07-29 18:38:23 +0300

    Fix unit tests errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-from app import ping
+from app import app
 
 
 def test_ping():
-    assert ping() == {"message": "pong"}
+    with app.test_client() as client:
+        response = client.get("/api/ping")
+        assert response.status_code == 200
+        data = response.get_json()
+        assert data["message"] == "pong"
+        assert "hostname" in data
